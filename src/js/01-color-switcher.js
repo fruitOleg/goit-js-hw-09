10e31114bcd4936e3da2a01ce14b8c5fdd6790ea
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-24 19:42:12 +0200

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('.start_button'),
+  stopBtn: document.querySelector('.stop_button'),
+  bodyStyle: document.querySelector('body'),
+};
+let timerId = null;
+refs.startBtn.addEventListener('click', onStartButtonClick);
+refs.stopBtn.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  timerId = setInterval(() => {
+    refs.bodyStyle.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
