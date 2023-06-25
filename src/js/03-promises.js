b451b2727eec75f2123a3d7b93fbe722e9a7f394
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-25 13:02:22 +0200

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const refs = {
+  delay: document.querySelector('#delay'),
+  step: document.querySelector('#step'),
+  amount: document.querySelector('#amount'),
+  createBtn: document.querySelector('.createBtn'),
+};
+
 function createPromise(position, delay) {
+  const obj = { position, delay };
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(obj);
+      } else {
+        // Reject
+        reject(obj);
+      }
+    }, 2000);
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
